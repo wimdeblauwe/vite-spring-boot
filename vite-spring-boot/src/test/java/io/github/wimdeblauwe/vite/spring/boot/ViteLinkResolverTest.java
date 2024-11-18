02ee0d9a3a9b1f52f798758ef6455f3da1420e94
Wim Deblauwe <wim.deblauwe@gmail.com>
2024-11-18 22:02:44 +0100

    test: Add test for ViteLinkResolver

---
+++ vite-spring-boot/src/test/java/io/github/wimdeblauwe/vite/spring/boot/ViteLinkResolverTest.java
@@ -0,0 +1,63 @@
+package io.github.wimdeblauwe.vite.spring.boot;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class ViteLinkResolverTest {
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Nested
+  class ResolveResourceTests {
+    @Test
+    void testResolveResourceInDevMode() throws IOException {
+      ViteLinkResolver resolver = createLinkResolver(ViteConfigurationProperties.Mode.DEV);
+      String resource = resolver.resolveResource("css/application.css");
+      assertThat(resource).isEqualTo("//localhost:5173/static/css/application.css");
+    }
+
+    @Test
+    void testResolveResourceInBuildMode() throws IOException {
+      ViteLinkResolver resolver = createLinkResolver(ViteConfigurationProperties.Mode.BUILD);
+      String resource = resolver.resolveResource("css/application.css");
+      assertThat(resource).isEqualTo("assets/application-BJA3xOLB.css");
+    }
+  }
+
+  @Nested
+  class GetManifestEntryTests {
+    @Test
+    void testDirectMatch() throws IOException {
+      ViteLinkResolver resolver = createLinkResolver(ViteConfigurationProperties.Mode.BUILD);
+      ViteManifestReader.ManifestEntry entry = resolver.getManifestEntry("css/application.css");
+      assertThat(entry.file()).isEqualTo("assets/application-BJA3xOLB.css");
+    }
+
+    @Test
+    void testIndirectMatch() throws IOException {
+      ViteLinkResolver resolver = createLinkResolver(ViteConfigurationProperties.Mode.BUILD);
+      ViteManifestReader.ManifestEntry entry = resolver.getManifestEntry("_client-3T5L5Tgj.js");
+      assertThat(entry.file()).isEqualTo("assets/client-3T5L5Tgj.js");
+    }
+  }
+
+  private ViteLinkResolver createLinkResolver(ViteConfigurationProperties.Mode mode) throws IOException {
+    ViteConfigurationProperties properties = new ViteConfigurationProperties(mode, new ClassPathResource("io/github/wimdeblauwe/vite/spring/boot/vite-manifest-example.json"));
+    ViteManifestReader manifestReader = new ViteManifestReader(objectMapper, properties);
+    manifestReader.init();
+    return new ViteLinkResolver(properties,
+            new ViteDevServerConfigurationProperties("localhost", 5173),
+            manifestReader);
+  }
+}
